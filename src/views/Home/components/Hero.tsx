c4ece21106f494b7569c2b1c768a99932a0a5b73
senior-developer1019 <seniordeveloper1019@outlook.com>
2021-10-02 07:49:56 +0900

    update the logo

--- src/views/Home/components/Hero.tsx
+++ src/views/Home/components/Hero.tsx
@@ -104,7 +104,7 @@ const Hero = () => {
       >
         <Flex flex="1" flexDirection="column">
           <Heading scale="xxl" color="secondary" mb="24px">
-            {t('No Loss Jackpot By LovePot.')}
+            {t('The moon is made of GS.')}
           </Heading>
           <Heading scale="md" mb="24px">
             {t('Make good earning crypto safely and securely on out decentralized finance platform by Staking in Pool, Farm and Jackpot.')}
